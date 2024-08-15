6db28b29e17b429940b31501bb4e63bbf629cda8
Alex Leifermann <aleif92@outlook.com>
2024-08-15 13:49:04 -0500

    assignment

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
+const firstName = `"Alex"`;
 
-// Exercise 3
+let lastname = "Leifermann";
+
+var age = 31;
+
+
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+let price = 19.99;
+
+let isOnSaple = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookMark = 456;
+
+let hasRead = true;
+
